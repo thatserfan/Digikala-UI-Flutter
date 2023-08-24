ac37eeb3700a19e056028cfeddfc234961aafde2
Erfan Farahbakhsh <itserfan.farahbakhsh@gmail.com>
2023-08-24 12:28:30 +0330

    Make image slider border

--- lib/widgets/image_slider.dart
+++ lib/widgets/image_slider.dart
@@ -1,18 +1,32 @@
 import 'package:flutter/material.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 
-class ImageSlider extends StatelessWidget {
+class ImageSlider extends StatefulWidget {
+  @override
+  State<ImageSlider> createState() => _ImageSliderState();
+}
+
+class _ImageSliderState extends State<ImageSlider> {
+  int _current = 0;
+  final CarouselController _controller = CarouselController();
+
   @override
   Widget build(BuildContext context) {
     return CarouselSlider(
-      options: CarouselOptions(height: 180.0),
+      carouselController: _controller,
+      options: CarouselOptions(
+        height: 180.0,
+        autoPlay: true,
+      ),
       items: [1, 2, 3, 4, 5].map((i) {
         return Builder(
           builder: (BuildContext context) {
             return Container(
                 width: MediaQuery.of(context).size.width,
                 margin: EdgeInsets.symmetric(horizontal: 5.0),
-                decoration: BoxDecoration(color: Colors.amber),
+                decoration: BoxDecoration(
+                    color: Colors.amber,
+                    borderRadius: BorderRadius.circular(20)),
                 child: Text(
                   'text $i',
                   style: TextStyle(fontSize: 16.0),
