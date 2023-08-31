ce27b2a6502c0a2a1126960e6ddcba7d3a5d9a2e
Erfan Farahbakhsh <itserfan.farahbakhsh@gmail.com>
2023-08-31 11:51:03 +0330

    Create model for sub category

---
+++ lib/models/subcategory.dart
@@ -0,0 +1,13 @@
+class SubCategory {
+  const SubCategory({
+    required this.id,
+    required this.title,
+    required this.mainCategory,
+    required this.imagePath,
+  });
+
+  final String id;
+  final String title;
+  final String mainCategory;
+  final String imagePath;
+}
