65995b4560462f1138395faabb09cea2336dd191
Erfan Farahbakhsh <itserfan.farahbakhsh@gmail.com>
2023-08-31 12:19:22 +0330

    Update subcategory data

---
+++ lib/data/subcategories.dart
@@ -0,0 +1,11 @@
+import 'package:digikala_ui/models/subcategory.dart';
+
+List<SubCategory> subCategories = const [
+  SubCategory(
+    id: 'phone',
+    title: 'موبایل',
+    mainCategory: 'phones',
+    imagePath: 'assets/image/products/phone.jpg',
+    amount: 10,
+  ),
+];
