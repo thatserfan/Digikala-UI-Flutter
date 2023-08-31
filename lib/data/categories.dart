67d8e79f214e3edbd0d511b951f82dfa808adb74
Erfan Farahbakhsh <itserfan.farahbakhsh@gmail.com>
2023-08-31 12:19:03 +0330

    Update category data

---
+++ lib/data/categories.dart
@@ -0,0 +1,7 @@
+import 'package:digikala_ui/models/category.dart';
+
+List<Category> categories = const [
+  Category(id: 'phones', title: 'موبایل'),
+  Category(id: 'digital-products', title: 'کالای دیجیتال'),
+  Category(id: 'home-kitchen', title: 'خانه و آشپزخانه'),
+];
