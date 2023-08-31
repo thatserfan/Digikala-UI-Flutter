8a31861aa0d3bc9dbf207a85fb3203260f50712f
Erfan Farahbakhsh <itserfan.farahbakhsh@gmail.com>
2023-08-31 12:20:17 +0330

    Add amount of subcategory model

--- lib/models/subcategory.dart
+++ lib/models/subcategory.dart
@@ -4,10 +4,12 @@ class SubCategory {
     required this.title,
     required this.mainCategory,
     required this.imagePath,
+    required this.amount,
   });
 
   final String id;
   final String title;
   final String mainCategory;
   final String imagePath;
+  final int amount;
 }
