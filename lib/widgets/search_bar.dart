a72d090c0f7af9c69cec5cb375bb43256448f03e
Erfan Farahbakhsh <itserfan.farahbakhsh@gmail.com>
2023-09-05 11:56:42 +0330

    Update search bar image

--- lib/widgets/search_bar.dart
+++ lib/widgets/search_bar.dart
@@ -12,25 +12,26 @@ class SearchBarWidget extends StatelessWidget {
         borderRadius: BorderRadius.circular(10),
         color: Colors.grey[200],
       ),
-      child: const Padding(
-        padding: EdgeInsets.symmetric(horizontal: 10),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
         child: Row(
           mainAxisAlignment: MainAxisAlignment.end,
           children: [
-            Text(
-              'دیجی کالا',
-              style: TextStyle(color: Colors.red),
-            ),
-            Text(
+            // Text(
+            //   'دیجی کالا',
+            //   style: TextStyle(color: Colors.red),
+            // ),
+            Image.asset('assets/image/type-logo.png', height: 20),
+            const Text(
               ' جستجو در',
               style: TextStyle(
                 color: Colors.grey,
               ),
             ),
-            Padding(
+            const Padding(
               padding: EdgeInsets.symmetric(horizontal: 5),
             ),
-            Icon(Icons.search),
+            const Icon(Icons.search),
           ],
         ),
       ),
