3460678c11b4b33b101818c98566d6dc586713a4
Erfan Farahbakhsh <itserfan.farahbakhsh@gmail.com>
2023-09-11 18:59:43 +0330

    Update data

--- lib/data/subcategories.dart
+++ lib/data/subcategories.dart
@@ -2,10 +2,39 @@ import 'package:digikala_ui/models/subcategory.dart';
 
 List<SubCategory> subCategories = const [
   SubCategory(
+    id: 'phone-covers',
+    title: 'کیف و کاور گوشی',
+    mainCategory: 'phones',
+    imagePath: 'assets/image/products/cover.png',
+    amount: 100,
+  ),
+  SubCategory(
     id: 'phone',
     title: 'موبایل',
     mainCategory: 'phones',
-    imagePath: 'assets/image/products/phone.jpg',
+    imagePath: 'assets/image/products/phone.png',
     amount: 10,
+  ),
+
+    SubCategory(
+    id: 'airpod',
+    title: 'ایرپاد',
+    mainCategory: 'digital-products',
+    imagePath: 'assets/image/products/airpod.png',
+    amount: 1000,
+  ),
+    SubCategory(
+    id: 'smart-watch',
+    title: 'ساعت هوشمند',
+    mainCategory: 'digital-products',
+    imagePath: 'assets/image/products/smart-watch.png',
+    amount: 1000,
+  ),
+    SubCategory(
+    id: 'laptop',
+    title: 'لپ تاپ',
+    mainCategory: 'digital-products',
+    imagePath: 'assets/image/products/laptop.png',
+    amount: 1000,
   ),
 ];
