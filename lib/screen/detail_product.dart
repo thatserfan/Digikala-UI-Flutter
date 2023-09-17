2f8b4a481c427bd372568b7198fb4ed2d73f687a
Erfan Farahbakhsh <itserfan.farahbakhsh@gmail.com>
2023-09-17 15:50:39 +0330

    Add button, price and offer time in detail product

--- lib/screen/detail_product.dart
+++ lib/screen/detail_product.dart
@@ -1,3 +1,4 @@
+import 'package:digikala_ui/widgets/special_offers_detail_timer.dart';
 import 'package:flutter/material.dart';
 
 class DetailProduct extends StatelessWidget {
@@ -27,7 +28,7 @@ class DetailProduct extends StatelessWidget {
         ),
         actions: [
           IconButton(
-            onPressed: () {},
+            onPressed: () => Navigator.of(context).pop(),
             icon: const Icon(Icons.close),
           ),
         ],
@@ -36,35 +37,109 @@ class DetailProduct extends StatelessWidget {
         elevation: 10,
         child: SizedBox(
           width: double.infinity,
-          height: 80,
+          height: 70,
           child: Row(
             children: [
               Padding(
                 padding: const EdgeInsets.symmetric(horizontal: 10),
                 child: Row(
                   children: [
-                    Text(
-                      'تومن',
-                      style: Theme.of(context)
-                          .textTheme
-                          .titleLarge
-                          ?.copyWith(fontWeight: FontWeight.bold),
-                    ),
-                    const SizedBox(
-                      width: 2,
-                    ),
-                    Text(
-                      '100.000',
-                      style: Theme.of(context)
-                          .textTheme
-                          .titleLarge
-                          ?.copyWith(fontWeight: FontWeight.bold),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Row(
+                          children: [
+                            Text(
+                              '500.000',
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .titleLarge
+                                  ?.copyWith(
+                                    color: Colors.grey,
+                                    decoration: TextDecoration.lineThrough,
+                                    decorationThickness: 1.5,
+                                  ),
+                            ),
+                            const SizedBox(width: 5),
+                            Container(
+                              decoration: BoxDecoration(
+                                color: Theme.of(context).colorScheme.secondary,
+                                borderRadius: BorderRadius.circular(20),
+                              ),
+                              child: Padding(
+                                padding: const EdgeInsets.symmetric(
+                                    horizontal: 8, vertical: 2),
+                                child: Text(
+                                  '80%',
+                                  style: Theme.of(context)
+                                      .textTheme
+                                      .labelMedium
+                                      ?.copyWith(
+                                        color: Theme.of(context)
+                                            .colorScheme
+                                            .background,
+                                      ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            Text(
+                              'تومن',
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .titleLarge
+                                  ?.copyWith(fontWeight: FontWeight.bold),
+                            ),
+                            const SizedBox(
+                              width: 2,
+                            ),
+                            Text(
+                              '100.000',
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .titleLarge
+                                  ?.copyWith(fontWeight: FontWeight.bold),
+                            ),
+                          ],
+                        ),
+                      ],
                     ),
                   ],
                 ),
               ),
+              const Expanded(child: SizedBox()),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 15),
+                child: ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Theme.of(context).colorScheme.secondary,
+                    foregroundColor: Theme.of(context).colorScheme.onSecondary,
+                    shape: ContinuousRectangleBorder(
+                        borderRadius: BorderRadius.circular(20)),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 15),
+                    child: Text(
+                      'افزودن به سبد خرید',
+                      style: Theme.of(context)
+                          .textTheme
+                          .titleLarge
+                          ?.copyWith(color: Colors.white),
+                    ),
+                  ),
+                ),
+              ),
             ],
           ),
+        ),
+      ),
+      body: const SingleChildScrollView(
+        child: Column(
+          children: [SpecialOffersDetailTimer()],
         ),
       ),
     );
