bcfc970116e923067f09b7cf4b77bb095725b9dd
Erfan Farahbakhsh <itserfan.farahbakhsh@gmail.com>
2023-09-18 15:06:34 +0330

    Make bottom sheet for detail product

--- lib/screen/detail_product.dart
+++ lib/screen/detail_product.dart
@@ -1,3 +1,4 @@
+import 'package:digikala_ui/widgets/detail_product_bottom_sheet.dart';
 import 'package:digikala_ui/widgets/special_offers_detail_timer.dart';
 import 'package:flutter/material.dart';
 
@@ -33,109 +34,9 @@ class DetailProduct extends StatelessWidget {
           ),
         ],
       ),
-      bottomSheet: Material(
-        elevation: 10,
-        child: SizedBox(
-          width: double.infinity,
-          height: 70,
-          child: Row(
-            children: [
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 10),
-                child: Row(
-                  children: [
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Row(
-                          children: [
-                            Text(
-                              '500.000',
-                              style: Theme.of(context)
-                                  .textTheme
-                                  .titleLarge
-                                  ?.copyWith(
-                                    color: Colors.grey,
-                                    decoration: TextDecoration.lineThrough,
-                                    decorationThickness: 1.5,
-                                  ),
-                            ),
-                            const SizedBox(width: 5),
-                            Container(
-                              decoration: BoxDecoration(
-                                color: Theme.of(context).colorScheme.secondary,
-                                borderRadius: BorderRadius.circular(20),
-                              ),
-                              child: Padding(
-                                padding: const EdgeInsets.symmetric(
-                                    horizontal: 8, vertical: 2),
-                                child: Text(
-                                  '80%',
-                                  style: Theme.of(context)
-                                      .textTheme
-                                      .labelMedium
-                                      ?.copyWith(
-                                        color: Theme.of(context)
-                                            .colorScheme
-                                            .background,
-                                      ),
-                                ),
-                              ),
-                            ),
-                          ],
-                        ),
-                        Row(
-                          children: [
-                            Text(
-                              'تومن',
-                              style: Theme.of(context)
-                                  .textTheme
-                                  .titleLarge
-                                  ?.copyWith(fontWeight: FontWeight.bold),
-                            ),
-                            const SizedBox(
-                              width: 2,
-                            ),
-                            Text(
-                              '100.000',
-                              style: Theme.of(context)
-                                  .textTheme
-                                  .titleLarge
-                                  ?.copyWith(fontWeight: FontWeight.bold),
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-              ),
-              const Expanded(child: SizedBox()),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 15),
-                child: ElevatedButton(
-                  onPressed: () {},
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Theme.of(context).colorScheme.secondary,
-                    foregroundColor: Theme.of(context).colorScheme.onSecondary,
-                    shape: ContinuousRectangleBorder(
-                        borderRadius: BorderRadius.circular(20)),
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 15),
-                    child: Text(
-                      'افزودن به سبد خرید',
-                      style: Theme.of(context)
-                          .textTheme
-                          .titleLarge
-                          ?.copyWith(color: Colors.white),
-                    ),
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ),
+      bottomSheet: const DetailProductBottomSheet(
+        price: 12600000,
+        beforePrice: 86000000,
       ),
       body: const SingleChildScrollView(
         child: Column(
---
+++ lib/widgets/detail_product_bottom_sheet.dart
@@ -0,0 +1,124 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+class DetailProductBottomSheet extends StatelessWidget {
+  const DetailProductBottomSheet(
+      {super.key, required this.price, this.beforePrice});
+
+  final int price;
+  final int? beforePrice;
+
+  @override
+  Widget build(BuildContext context) {
+    final formater = NumberFormat("#,##0");
+    String formatPrice = formater.format(price);
+    String? formatBeforePrice =
+        beforePrice != null ? formater.format(beforePrice) : null;
+
+    return Material(
+      elevation: 10,
+      child: SizedBox(
+        width: double.infinity,
+        height: 70,
+        child: Row(
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              child: Row(
+                children: [
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      if (formatBeforePrice != null)
+                        Row(
+                          children: [
+                            Text(
+                              beforePrice.toString(),
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .titleLarge
+                                  ?.copyWith(
+                                    color: Colors.grey,
+                                    decoration: TextDecoration.lineThrough,
+                                    decorationThickness: 1.5,
+                                  ),
+                            ),
+                            const SizedBox(width: 5),
+                            Container(
+                              decoration: BoxDecoration(
+                                color: Theme.of(context).colorScheme.secondary,
+                                borderRadius: BorderRadius.circular(20),
+                              ),
+                              child: Padding(
+                                padding: const EdgeInsets.symmetric(
+                                    horizontal: 8, vertical: 2),
+                                child: Text(
+                                  '${(((beforePrice! - price) / beforePrice!.abs()) * 100).toInt()}%',
+                                  style: Theme.of(context)
+                                      .textTheme
+                                      .labelMedium
+                                      ?.copyWith(
+                                        color: Theme.of(context)
+                                            .colorScheme
+                                            .background,
+                                      ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      Row(
+                        children: [
+                          Text(
+                            'تومن',
+                            style: Theme.of(context)
+                                .textTheme
+                                .titleMedium
+                                ?.copyWith(fontWeight: FontWeight.bold),
+                          ),
+                          const SizedBox(
+                            width: 4,
+                          ),
+                          Text(
+                            formatPrice,
+                            style: Theme.of(context)
+                                .textTheme
+                                .titleLarge
+                                ?.copyWith(fontWeight: FontWeight.bold),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            const Expanded(child: SizedBox()),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 15),
+              child: ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Theme.of(context).colorScheme.secondary,
+                  foregroundColor: Theme.of(context).colorScheme.onSecondary,
+                  shape: ContinuousRectangleBorder(
+                      borderRadius: BorderRadius.circular(20)),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 13),
+                  child: Text(
+                    'افزودن به سبد خرید',
+                    style: Theme.of(context)
+                        .textTheme
+                        .titleLarge
+                        ?.copyWith(color: Colors.white),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
